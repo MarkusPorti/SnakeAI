022efb120051cca15ab402c589f54067a46c72a6
Markus <markus.portugall@gmx.de>
2020-05-22 17:11:26 +0200

    AI

--- snake_game/Game.py
+++ snake_game/Game.py
@@ -1,21 +1,31 @@
 import pygame
-from random import randint
+from random import randint, random
 from snake_game.Snake import Snake
+
+import numpy as np
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+import collections
+import random
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class Game:
     def __init__(self, width=20, height=20, gui=False):
         self.gui = gui
         self.pixel = 20
-        self.snake = Snake(width, height, self.pixel)
         self.food = []
         self.running = True
         self.moves_left = width * height
         self.board = {'width': width, 'height': height}
         self.food_color = (255, 0, 0)
         self.clock = pygame.time.Clock()
-
-    def start(self):
+        self.running = True
+        self.snake = Snake(self.board['width'], self.board['height'], self.pixel)
         self.generate_food()
         if self.gui:
             self.render_init()
@@ -51,47 +61,129 @@ class Game:
             self.generate_food()
         if self.gui:
             self.render()
-        return self.running, self.snake
+        return self.get_state()
 
     def generate_food(self):
         food = []
         while food == []:
-            food = [randint(0, self.board["width"]-1), randint(0, self.board["height"]-1)]
+            food = [randint(0, self.board["width"] - 1), randint(0, self.board["height"] - 1)]
             if food in self.snake:
                 food = []
         self.food = food
 
+    def get_state(self):
+        return self.running, self.moves_left, self.snake, self.__get_map()
+
+    def __get_map(self):
+        map = []
+        for w in range(self.board['width']):
+            map.append([])
+            for h in range(self.board['height']):
+                map[w].append(0)
+                if [w, h] in self.snake:
+                    map[w][h] = 1
+                elif [w, h] == self.food:
+                    map[w][h] = 2
+                else:
+                    map[w][h] = 0
+        return np.array(map).flatten()
+
+
+def create_model():
+    model = Sequential()
+    model.add(Dense(units=400, activation='relu', input_dim=400))
+    model.add(Dense(units=400, activation='relu'))
+    model.add(Dense(units=400, activation='relu'))
+    model.add(Dense(units=4, activation='softmax'))
+    opt = Adam(0.005)
+    model.compile(loss='mse', optimizer=opt)
+    return model
+
+
+def remember(state, action, reward, next_state, running):
+    memory.append((state, action, reward, next_state, running))
+
+
+def train_short_memmory(state, action, reward, next_state, running):
+    target = reward
+    if running:
+        target = reward + np.amax(model.predict(next_state[3].reshape((1, 400))))
+    target_f = model.predict(state[3].reshape((1, 400)))
+    target_f[0][np.argmax(action)] = target
+    model.fit(state[3].reshape((1, 400)), target_f, epochs=1, verbose=0)
+
+
+def train_long_memmory(batch_size):
+    if len(memory) > batch_size:
+        minibatch = random.sample(memory, batch_size)
+    else:
+        minibatch = memory
+    for state, action, reward, next_state, running in minibatch:
+        target = reward
+        if running:
+            target = reward + np.amax(model.predict(np.array([next_state[3]]))[0])
+        target_f = model.predict(np.array([state[3]]))
+        target_f[0][np.argmax(action)] = target
+        model.fit(np.array([state[3]]), target_f, epochs=1, verbose=0)
+
+
+def plot_seaborn(array_counter, array_score):
+    sns.set(color_codes=True)
+    ax = sns.regplot(
+        np.array([array_counter])[0],
+        np.array([array_score])[0],
+        color="b",
+        x_jitter=.1,
+        line_kws={'color': 'green'}
+    )
+    ax.set(xlabel='games', ylabel='score')
+    plt.show()
+
+
+def run(episodes=300):
+    global model, memory
+    model = create_model()
+    memory = collections.deque(maxlen=3000)
+
+    score_plot = []
+    counter_plot = []
+
+    for counter_games in range(episodes):
+        print('Simulation ', counter_games, ' out of ', str(episodes), '\r', end='')
+        game = Game(gui=True)
+
+        while game.running:
+            # game.clock.tick(20)
+
+            # get old state
+            state_old = game.get_state()
+
+            # at the beginning more random, lately more advised actions
+            if randint(0, 1) < 1 - (counter_games * 1/25):
+                final_move = randint(0, 3)
+            else:
+                prediction = model.predict(state_old[3].reshape((1, 400)))
+                final_move = np.argmax(prediction[0])
+
+            game.snake.move(final_move)
+            state_new = game.step()
+            reward = 0
+            if not state_new[0]:
+                reward = -10
+            elif state_new[2].score > state_old[2].score:
+                reward = 20
+
+            # train short
+            train_short_memmory(state_old, final_move, reward, state_new, state_new[0])
+            remember(state_old, final_move, reward, state_new, state_new[0])
+
+        # train long
+        train_long_memmory(500)
+
+        score_plot.append(game.snake.score)
+        counter_plot.append(counter_games)
+    plot_seaborn(counter_plot, score_plot)
+
 
 if __name__ == "__main__":
-    game = Game(gui=True)
-    game.start()
-
-    while game.running:
-        # Framerate
-        game.clock.tick(10)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_UP:
-            #         self.snake.move(0)
-            #     elif event.key == pygame.K_RIGHT:
-            #         self.snake.move(1)
-            #     elif event.key == pygame.K_DOWN:
-            #         self.snake.move(2)
-            #     elif event.key == pygame.K_LEFT:
-            #         self.snake.move(3)
-            #     break
-
-        game.snake.move(randint(0, 3))
-
-        game.step()
-
-    while not game.running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    run()
