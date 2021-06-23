2618ee9f5de9c022d12d8e089a6ffd353052c328
Markus <markus.portugall@gmx.de>
2021-06-23 22:11:24 +0200

    Umbau auf Torch

--- agent.py
+++ agent.py
@@ -1,141 +1,58 @@
-import time
+import random
+from collections import deque
 
 import numpy as np
-import pygame
-from keras import layers
-from keras.layers.core import Dense, Flatten
-from keras.models import Sequential, Model
-from keras.optimizers import RMSprop
+import torch
 
-from experience_replay import ExperienceReplay, Experience
-from game import Game
+from model import DQN, QTrainer
+
+DEVICE = torch.device("cpu")
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
 
 
 class Agent:
-    def __init__(self):
-        self.gamma = 0.99
-        self.batch_size = 100
-        self.learning_rate = 1e-4
-        self.eps_start = 1.
+    def __init__(self, gym_env):
+        self.gamma = 0.9
+        self.epsilon = 1.
         self.eps_decacy = .999955
         self.eps_min = .02
 
-        self.exp_buffer = ExperienceReplay(capacity=2500)
-        self.env = Game(12, 8, gui=True)
-        self.model: Model = self.__create_model()
-        self.losses = []
-        self._reset()
+        self.n_games = 0
+        self.env = gym_env
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = DQN(self.env.observation_space.shape, self.env.action_space.n).to(DEVICE)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+        print(self.model)
 
-    def _reset(self):
-        self.state, _, _ = self.env.reset_game()
-        self.total_reward = 0.0
+    def remember(self, state, action, reward, next_state, done):
+        # popleft if MAX_MEMORY is reached
+        self.memory.append((state, action, reward, next_state, done))
 
-    def __create_model(self):
-        _model = Sequential()
-        _model.add(layers.Convolution2D(16, (3, 3), activation='relu',
-                                        input_shape=self.env.get_input_shape()))
-        _model.add(layers.Convolution2D(32, (1, 1), activation='relu'))
-        _model.add(Flatten())
-        _model.add(Dense(256, activation='relu'))
-        _model.add(Dense(4))
-        _model.compile(RMSprop(), 'MSE')
-        return _model
-
-    def train(self, epochs=10000):
-        epsilon = self.eps_start
-        total_rewards = []
-
-        for frame_idx in range(epochs):
-            # print('Simulation: ', frame_idx, '\tScore: ', max_score, end='\r')
-            # if epsilon > self.eps_min:
-            #     epsilon -= .9 / (epochs / 2)
-
-            # run one game till snake dies
-            while self.env.running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        quit()
-
-                epsilon = max(epsilon * self.eps_decacy, self.eps_min)
-
-                # total reward for this game
-                reward = self.__play_step(epsilon)
-
-                if reward is not None:
-                    total_rewards.append(reward)
-                    mean_reward = np.mean(total_rewards[-100:])
-                    print("Epoche %d: avg. reward %.3f, (epsilon %.4f)" %
-                          (frame_idx, mean_reward, epsilon))
-
-            self._reset()
-
-            if len(self.exp_buffer) < self.batch_size:
-                continue
-
-            # tic = time.perf_counter()
-            minibatch = self.exp_buffer.sample(self.batch_size)
-            states = []
-            targets = []
-            for state, action, reward, done, next_state in minibatch:
-                target = reward
-                if not done:
-                    target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-                target_f = self.model.predict(state)
-                target_f[0][np.argmax(action)] = target
-                states.append(state.reshape(self.env.get_input_shape()))
-                targets.append(target_f[0])
-            self.model.train_on_batch(np.array(states), np.array(targets))
-            # print("Train/Fit Model took %.4fs" % (time.perf_counter() - tic), end='\n\n')
-
-        # save current weights of self.model
-        self.model.save_weights('checkpoints/my_checkpoint')
-
-    def __play_step(self, epsilon=.0):
-        done_reward = None
-        if np.random.random() < epsilon:
-            action = self.env.step_random()
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)  # list of tuples
         else:
-            prediction = self.model.predict(self.state)
-            action = np.argmax(prediction[0])
+            mini_sample = self.memory
 
-        new_state, reward, is_done = self.env.step(action)
-        self.total_reward += reward
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        # for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
 
-        exp = Experience(self.state, action, reward, is_done, new_state)
-        self.exp_buffer.append(exp)
-        self.state = new_state
-        if is_done:
-            done_reward = self.total_reward
-        return done_reward
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
 
-    def play_manual(self, rounds=1):
-        for counter_games in range(rounds):
-            print('Game ', counter_games, '\r', end='')
-            self.env.reset_game()
+    def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = max(self.epsilon * self.eps_decacy, self.eps_min)
+        if np.random.random() < self.epsilon:
+            final_move = self.env.action_space.sample()
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move = move
 
-            final_move = 1
-            # run one game till snake dies
-            while self.env.running:
-                self.env.clock.tick(5)
-
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        quit()
-
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_DOWN:
-                            final_move = 0
-                        elif event.key == pygame.K_UP:
-                            final_move = 1
-                        elif event.key == pygame.K_RIGHT:
-                            final_move = 2
-                        elif event.key == pygame.K_LEFT:
-                            final_move = 3
-                        break
-
-                self.env.step(final_move)
-
-        pygame.quit()
-        quit()
+        return final_move
---
+++ model.py
@@ -0,0 +1,88 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class DQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 16, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(16, 64, kernel_size=(2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=(1, 1)),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
+
+        if len(state.shape) == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            q_new = reward[idx]
+            if not done[idx]:
+                q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action[idx]).item()] = q_new
+
+        # 2: q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
+
